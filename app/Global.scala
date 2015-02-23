81415ea079bbc8c90fe802cf546d4794392e0924
yugolf <yugo.maede@gmail.com>
2015-02-23 13:50:09 +0900

    #2 delete the logic which accesses to "CODE_MASTER".

--- app/Global.scala
+++ app/Global.scala
@@ -1,16 +1,13 @@
 import play.api._
 import play.cache.Cache
-import daos.common.CodeMasters
 
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.info("Application has started")
-    Cache.set("codeMasters", CodeMasters.getCodeMaster)
   }
 
   override def onStop(app: Application) {
     Logger.info("Application shutdown...")
-    Cache.remove("codeMasters")
   }
 }
